abf46e502f437ae30acf8c92beb8f7f98f15f114
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-21 21:38:56 +0700

    class mahasiswa

--- initialisasi.cpp
+++ initialisasi.cpp
@@ -8,23 +8,3 @@ private:
 	string nama;
 	float nilai;
 
-public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
-	{}
-
-		~mahasiswa() {
-			cout << "Id = " << id << endl;
-			cout << "Nama = " << nama << endl;
-			cout << "Nilai = " << nilai << endl;
-
-
-
-		}
-
-};
-
-int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
-
-	return 0;
-}
