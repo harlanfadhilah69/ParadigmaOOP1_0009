ae3dd19e6d1b9dd31eb221c9f18b45bdfb47ebb3
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-21 17:40:42 +0700

    cout id nama nilai

--- initialisasi.cpp
+++ initialisasi.cpp
@@ -9,5 +9,14 @@ private:
 	float nilai;
 
 public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai);
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+	{}
+
+		~mahasiswa() {
+			cout << "Id = " << id << endl;
+			cout << "Nama = " << nama << endl;
+			cout << "Nilai = " << nilai << endl;
+		}
+
 };
+
